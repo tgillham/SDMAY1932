09c18a2aa539fa7dd54c753a1b11dad133a25a83
Travis Gillham <35347134+tgillham@users.noreply.github.com>
2019-04-12 08:52:31 -0500

    Add files via upload

--- Senior_Design.ino
+++ Senior_Design.ino
@@ -12,17 +12,18 @@ const int ledPin =  13;      // the number of the LED pin
 int buttonState = 0;         // variable for reading the pushbutton status
 ////END OF STUFF THAT WILL BE DELETED WHEN WE GET TABLET INTERFACE GOING////
 
-unsigned long timer = 0;              //Use this for variable for millis(); INSTEAD of delay();
+unsigned long timer = 0;     //Use this for variable for millis(); INSTEAD of delay();
 
 void setup() {
 
-  Wire.begin(); // join i2c bus (address optional for master) 
+  Wire.begin(); // join i2c bus (address optional for master)
   Serial.begin(9600);
+  
   ////START OF STUFF THAT WILL BE DELETED WHEN WE GET TABLET INTERFACE GOING////
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   ////END OF STUFF THAT WILL BE DELETED WHEN WE GET TABLET INTERFACE GOING////
-  
+
 }
 
 
@@ -32,14 +33,14 @@ void setup() {
 //Tap_Position is a the value of slider variable  (0-40)
 
 void Volume(unsigned int Tap_Position_Val) {
-  
-            //==========================================================================
-            Wire.beginTransmission(0x28); // transmit to device #40
-            // device address is specified in datasheet
-            Wire.write(byte(0xa9)); // sends instruction byte 
-            Wire.write(Tap_Position_Val); // sends potentiometer value byte 
-            Wire.endTransmission(); // stop transmitting
-            //==========================================================================
+
+  //==========================================================================
+  Wire.beginTransmission(0x28); // transmit to device #40
+  // device address is specified in datasheet
+  Wire.write(byte(0xa9)); // sends instruction byte
+  Wire.write(Tap_Position_Val); // sends potentiometer value byte
+  Wire.endTransmission(); // stop transmitting
+  //==========================================================================
 }
 
 //ADSR FUNCTION
@@ -48,7 +49,19 @@ void Volume(unsigned int Tap_Position_Val) {
 //Sustain is a the value of slider variable     (0-255)
 //Release is a the value of slider variable     (0-255)
 
-void ADSR(int Attack, int Decay, int Sustain, int Release) {
+void ADSR(int Attack, int Decay, int Sustain, int Release, int MIDI_Note) {
+
+  //Tells What Note Is/Has Been Pressed ----> FROM GUI
+  int MIDI_Note1 = MIDI_Note;  // --->STARTING GUI VALUE
+  int MIDI_Note_GUI = 0; // --->GUI VALUE UPDATED   //0 WILL BE REPLACED BY GUI VALUE
+
+  //==========================================================================
+  Wire.beginTransmission(0x28); // transmit to device #40
+  // device address is specified in datasheet
+  Wire.write(byte(0xaa)); // sends instruction byte
+  Wire.write(abs(0)); // sends potentiometer value byte
+  Wire.endTransmission(); // stop transmitting
+  //==========================================================================
 
   //Position is a the Tap Position of DIGI-POT  (0-255)
   int Position = 0;
@@ -57,106 +70,127 @@ void ADSR(int Attack, int Decay, int Sustain, int Release) {
   int Value = 0;
 
   //Note Tells if a Key Has Been Pressed      (0-1)
-  int Note; 
+  int Note;
 
-    //Periods for Attack, Decay, and Release
-    float Period_Attack = (Attack * (Max_Time_Attack/Tap_Positions))/Tap_Positions;
-    float Period_Decay = (Max_Time_Decay)/(Tap_Positions-Sustain);
-    float Period_Release = (Max_Time_Release)/(Sustain);
+  //Periods for Attack, Decay, and Release
+  float Period_Attack = (Attack * (Max_Time_Attack / Tap_Positions)) / Tap_Positions;
+  float Period_Decay = (Max_Time_Decay) / (Tap_Positions - Sustain);
+  float Period_Release = (Max_Time_Release) / (Sustain);
 
-    // Attack Section
-        for (Position= 0; Position < Tap_Positions-1; Position ++ ) {
-            timer = millis();
-            
-            if (Attack == 0){
-                Position = Tap_Positions-1;
-            }
+  // Attack Section
+  for (Position = 0; Position < Tap_Positions - 1; Position ++ ) {
+    timer = millis();
 
-            while(millis() < timer + Period_Attack){
+    if (Attack == 0) {
+      Position = Tap_Positions - 1;
+    }
 
-            }
+    //if timer > Period_Attack{
+    while (millis() < timer + Period_Attack) {
+      //Serial.println(timer);
+    }
 
-            //==========================================================================
-            Wire.beginTransmission(0x28); // transmit to device #40
-            // device address is specified in datasheet
-            Wire.write(byte(0xaa)); // sends instruction byte 
-            Wire.write(abs(Position)); // sends potentiometer value byte 
-            Wire.endTransmission(); // stop transmitting
-            //==========================================================================
-
-            Serial.println(abs(Position),DEC);        //Prints Tap Position
-            
-        }
-
-    // Decay Section
-        for (Position = Tap_Positions-1; Position >= Sustain+1; Position --) {
-            if (Decay == 0){
-                Position = Sustain+1;
-            }
-            timer = millis();
-
-            while(millis() < timer + Period_Decay){
-
-            }
-            
-            //==========================================================================
-            Wire.beginTransmission(0x28); // transmit to device #40
-            // device address is specified in datasheet
-            Wire.write(byte(0xaa)); // sends instruction byte 
-            Wire.write(abs(Position)); // sends potentiometer value byte 
-            Wire.endTransmission(); // stop transmitting
-            //==========================================================================
-
-            Serial.println(abs(Position),DEC);        //Prints Tap Position
-            
-        }
-
-    // Sustain Section
-        for (Value = 0; Value < 2; 1) {
-
-         //Read the State of the Pushbutton Value: --> WILL BE DELETED WHEN WE GET TABLET INTERFACE GOING
-         buttonState = digitalRead(buttonPin);
-
-         //Is Button Pressed?
-         if (buttonState == HIGH) {
-            Note = 1;       //YES
-            Value = 0;
-            Serial.println("Hi-------------------------");        //Prints Tap Position
-
-
-          } else {
-            Note = 0;       //NO
-            Value = 3;
-          }
-          
-            Serial.println(abs(Position),DEC);        //Prints Tap Position
-            
-        }
-
-    // Release Section
-        for (Position = Sustain; Position >= 0; Position --) {
-
-            
-            if (Release == 0){
-                Position = 0;
-            }
-            
-            timer = millis();
-
-            while(millis() < timer + Period_Release){
+    //==========================================================================
+    Wire.beginTransmission(0x28); // transmit to device #40
+    // device address is specified in datasheet
+    Wire.write(byte(0xaa)); // sends instruction byte
+    Wire.write(abs(Position)); // sends potentiometer value byte
+    Wire.endTransmission(); // stop transmitting
+    //==========================================================================
     
-            }
+    //If Note That Was Sent Is Not Same As Note Now Being Shown Start It All Over Again
+    if (MIDI_Note1 != MIDI_Note_GUI) {        
+      ADSR(Attack, Decay, Sustain, Release, MIDI_Note_GUI);
+    }
 
-            //==========================================================================
-            Wire.beginTransmission(0x28); // transmit to device #40
-            // device address is specified in datasheet
-            Wire.write(byte(0xaa)); // sends instruction byte 
-            Wire.write(abs(Position)); // sends potentiometer value byte 
-            Wire.endTransmission(); // stop transmitting
-            //==========================================================================
-            
-            Serial.println(abs(Position),DEC);        //Prints Tap Position
-        }
+    Serial.println(abs(Position), DEC);       //Prints Tap Position
+
+  }
+
+  // Decay Section
+  for (Position = Tap_Positions - 1; Position >= Sustain + 1; Position --) {
+    if (Decay == 0) {
+      Position = Sustain + 1;
+    }
+    timer = millis();
+
+    while (millis() < timer + Period_Decay) {
+
+    }
+
+    //==========================================================================
+    Wire.beginTransmission(0x28); // transmit to device #40
+    // device address is specified in datasheet
+    Wire.write(byte(0xaa)); // sends instruction byte
+    Wire.write(abs(Position)); // sends potentiometer value byte
+    Wire.endTransmission(); // stop transmitting
+    //==========================================================================
+    
+    //If Note That Was Sent Is Not Same As Note Now Being Shown Start It All Over Again
+    if (MIDI_Note1 != MIDI_Note_GUI) {
+      ADSR(Attack, Decay, Sustain, Release, MIDI_Note_GUI);
+    }
+
+    Serial.println(abs(Position), DEC);       //Prints Tap Position
+
+  }
+
+  // Sustain Section
+  for (Value = 0; Value < 2; 1) {
+
+    //Read the State of the Pushbutton Value: --> WILL BE DELETED WHEN WE GET TABLET INTERFACE GOING
+    buttonState = digitalRead(buttonPin);
+
+    //Is Button Pressed?
+    if (buttonState == HIGH) {
+      Note = 1;       //YES
+      Value = 0;
+      Serial.println("Hi-------------------------");        //Prints Tap Position
+
+
+    } else {
+      Note = 0;       //NO
+      Value = 3;
+    }
+
+    //If Note That Was Sent Is Not Same As Note Now Being Shown Start It All Over Again
+    if (MIDI_Note1 != MIDI_Note_GUI) {
+      ADSR(Attack, Decay, Sustain, Release, MIDI_Note_GUI);
+    }
+
+    Serial.println(abs(Position), DEC);       //Prints Tap Position
+
+  }
+
+  // Release Section
+  for (Position = Sustain; Position >= 0; Position --) {
+
+
+    if (Release == 0) {
+      Position = 0;
+    }
+
+    timer = millis();
+
+    while (millis() < timer + Period_Release) {
+
+    }
+
+    //==========================================================================
+    Wire.beginTransmission(0x28); // transmit to device #40
+    // device address is specified in datasheet
+    Wire.write(byte(0xaa)); // sends instruction byte
+    Wire.write(abs(Position)); // sends potentiometer value byte
+    Wire.endTransmission(); // stop transmitting
+    //==========================================================================
+
+    //If Note That Was Sent Is Not Same As Note Now Being Shown Start It All Over Again
+    if (MIDI_Note1 != MIDI_Note_GUI) {
+      ADSR(Attack, Decay, Sustain, Release, MIDI_Note_GUI);
+    }
+
+    Serial.println(abs(Position), DEC);       //Prints Tap Position
+  }
 }
 
 //VOLUME dB FUNCTION
@@ -167,14 +201,14 @@ unsigned int dB_to_tap(double dB) {
   unsigned int closest_tap;
   double raw_value;
 
-  if(dB >= 0)
+  if (dB >= 0)
   {
     closest_tap = 1;
   }
   else
   {
-    raw_value = total_taps - (pow(10,((dB/20)))*(total_taps-1));
-    closest_tap = (unsigned int) (raw_value+0.5); 
+    raw_value = total_taps - (pow(10, ((dB / 20))) * (total_taps - 1));
+    closest_tap = (unsigned int) (raw_value + 0.5);
   }
   return closest_tap;
 }
@@ -187,25 +221,28 @@ unsigned int dB_to_tap(double dB) {
 void loop() {
 
   //Volume_Value is a Place Holder Value to Compare to the Volume Slider (-40-0) 0 being the loudest
-  int Volume_Value = 0;     
+  int Volume_Value = 0;
 
   //Volume_Level is a the value of slider variable  (0-255)
-  int Volume_Level; // "SLIDER VALUE";  
+  int Volume_Level; // "SLIDER VALUE";
+
+  //Tells What Note is/has Been Pressed ----> FROM GUI
+  int MIDI_Note = 0;
 
   //Attack is a the value of slider variable      (0-255)
-  float Attack;  // "SLIDER VALUE";  
+  float Attack;  // "SLIDER VALUE";
 
   //Decay is a the value of slider variable     (0-255)
-  float Decay;   // "SLIDER VALUE"; 
+  float Decay;   // "SLIDER VALUE";
 
   //Sustain is a the value of slider variable     (0-255)
-  float Sustain; // "SLIDER VALUE"; 
+  float Sustain; // "SLIDER VALUE";
 
   //Release is a the value of slider variable     (0-255)
-  float Release; // "SLIDER VALUE"; 
+  float Release; // "SLIDER VALUE";
 
   //Note Tells if a Key Has Been Pressed        (0-1)
-  int Note = 0;  // "Value that Will Go High When Key Is Pressed"; 
+  int Note = 0;  // "Value that Will Go High When Key Is Pressed";
 
   //Read the State of the Pushbutton Value: --> WILL BE DELETED WHEN WE GET TABLET INTERFACE GOING
   buttonState = digitalRead(buttonPin);
@@ -222,20 +259,22 @@ void loop() {
   } else {
     Note = 0;         //NO
   }
+  
   Volume_Level = 1;
+  
   //If Volume Slider Is Changed
-  if(Volume_Value != Volume_Level)
+  if (Volume_Value != Volume_Level)
   {
-      unsigned int a = dB_to_tap(Volume_Level);
-      Volume(a);
-      Volume_Value = Volume_Level;      //NOW SET IT EQUAL TO VOLUME
+    unsigned int a = dB_to_tap(Volume_Level);
+    Volume(a);
+    Volume_Value = Volume_Level;      //NOW SET IT EQUAL TO VOLUME
   }
 
   //Key is/was Pressed
-  if(Note == 1)
+  if (Note == 1)
   {
-      ADSR(Attack,Decay,Sustain,Release);
-      Note = 0;
+    ADSR(Attack, Decay, Sustain, Release, MIDI_Note);
+    Note = 0;
   }
 
 }
